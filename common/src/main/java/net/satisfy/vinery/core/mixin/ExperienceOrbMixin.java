1638a0bd5a2baca47b8c7f29eb94a995dcbb1c1a
satisfy <104036234+satisfyu@users.noreply.github.com>
2025-01-25 08:48:08 +0100

    feat(fix): #Issue 454
    https://github.com/satisfyu/Let-s-Do-Hub/issues/454

--- common/src/main/java/net/satisfy/vinery/core/mixin/ExperienceOrbMixin.java
+++ common/src/main/java/net/satisfy/vinery/core/mixin/ExperienceOrbMixin.java
@@ -8,30 +8,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.lang.reflect.Field;
 import java.util.Objects;
 
 @Mixin(ExperienceOrb.class)
 public abstract class ExperienceOrbMixin {
 
-    @Inject(method = "playerTouch", at = @At("HEAD"))
+    @Inject(method = "playerTouch", at = @At("TAIL"))
     public void onPlayerTouch(Player player, CallbackInfo ci) {
         if (player.hasEffect(MobEffectRegistry.EXPERIENCE_EFFECT.get())) {
             int amplifier = Objects.requireNonNull(player.getEffect(MobEffectRegistry.EXPERIENCE_EFFECT.get())).amplifier;
             ExperienceOrb self = (ExperienceOrb) (Object) this;
 
-            try {
-                Field valueField = ExperienceOrb.class.getDeclaredField("value");
-                valueField.setAccessible(true);
+            int originalXp = self.getValue();
 
-                int originalXp = (int) valueField.get(self);
-
-                int modifiedXp = (int) (originalXp + (originalXp * amplifier * 0.5));
-
-                valueField.set(self, modifiedXp);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                throw new RuntimeException("Failed to access ExperienceOrb value field", e);
-            }
+            int bonusXp = (int) (originalXp * amplifier * 0.5);
+            player.giveExperiencePoints(bonusXp);
         }
     }
 }
